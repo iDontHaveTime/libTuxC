255b44232359c28e6b55a190bf2d302c86d6110b
iDontHaveTime <cheesedeveloperworking@gmail.com>
2025-07-21 16:24:39 +0300

    fixed rsp being offset by locals

--- crt/runtime.c
+++ crt/runtime.c
@@ -7,18 +7,30 @@ extern int main(int, char**);
 uintptr_t __stack_chk_guard;
 
 void _start(){
+    __asm__ volatile(
+        "movq %%rsp, %%r11\n"
+        "pushq %%rbx\n"
+        "movq %%r11, %%rbx"
+        :
+        :
+        : "r11", "rbx"
+    );
+
     __stack_chk_guard = __stack_chk_guard_gen();
     
     uintptr_t argc;
     char** argv;
 
-    __asm__ volatile (
-        "movq (%%rsp), %0\n"
-        "leaq 8(%%rsp), %1\n"
+    __asm__ volatile(
+        "movq (%%rbx), %0\n"
+        "leaq 8(%%rbx), %1\n"
         : "=r"(argc), "=r"(argv)
     );
     int exitCode = main(argc, argv);
 
+    __asm__ volatile(
+        "popq %%rbx"
+    );
     exit(exitCode);
 }
 
